c431f8b39303081c8fe21475ede363c6819d827f
leo <leoventura06@outlook.it>
2024-10-26 21:04:54 +0200

    update Work project with Booklet Online Book Store

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -3,6 +3,7 @@ import { Element } from "react-scroll";
 import boss from "../assets/boss.png";
 import logo from "../assets/user.png";
 import spotify from "../assets/spotify.jpeg";
+import booklet from "../assets/booklet.png";
 
 const Work: React.FC = () => {
     return <Element name="work" className="bg-[#0a192f] w-full md:h-screen text-gray-300">
@@ -16,18 +17,23 @@ const Work: React.FC = () => {
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
 
                 {/* grid template */}
-                <div style={{ backgroundImage: `url(${logo})` }} className="shadow-ls shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
-                    
+                <div style={{backgroundImage: `url(${logo})`}}
+                     className="shadow-ls shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+
                     {/* Hover effect */}
                     <div className="opacity-0 group-hover:opacity-100">
                         <span className="text-2xl font-bold text-white tracking-widee">
                             Portfolio
                         </span>
                         <div className="pt-8 text-center">
-                            <a href="/" className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg" rel="noopener noreferrer" target="_blank">
+                            <a href="/"
+                               className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+                               rel="noopener noreferrer" target="_blank">
                                 <button>Demo</button>
                             </a>
-                            <a href="https://github.com/l30-il-l30/portfolio" className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg" rel="noopener noreferrer" target="_blank">
+                            <a href="https://github.com/l30-il-l30/portfolio"
+                               className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+                               rel="noopener noreferrer" target="_blank">
                                 <button>Code</button>
                             </a>
                         </div>
@@ -35,37 +41,62 @@ const Work: React.FC = () => {
                 </div>
 
                 {/* grid template */}
-                <div style={{ backgroundImage: `url(${boss})` }} className="shadow-ls shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
-                    
+                <div style={{backgroundImage: `url(${boss})`}} className="shadow-ls shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+
                     {/* Hover effect */}
                     <div className="opacity-0 group-hover:opacity-100">
                         <span className="text-2xl font-bold text-white tracking-widee">
-                            React JS Application
+                            BOSS main website
                         </span>
                         <div className="pt-8 text-center">
-                            <a href="http://www.bossaccademy.it" className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg" rel="noopener noreferrer" target="_blank">
+                            <a href="http://www.bossaccademy.it"
+                               className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+                               rel="noopener noreferrer" target="_blank">
                                 <button>Demo</button>
                             </a>
-                            <a href="https://github.com/l30-il-l30/B.O.S.S." className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg" rel="noopener noreferrer" target="_blank">
+                            <a href="https://github.com/l30-il-l30/B.O.S.S."
+                               className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+                               rel="noopener noreferrer" target="_blank">
                                 <button>Code</button>
                             </a>
                         </div>
                     </div>
                 </div>
 
-                {/* grid template */}
-                <div style={{ backgroundImage: `url(${spotify})` }} className="shadow-ls shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
-
-                    {/* Hover effect */}
+                <div style={{backgroundImage: `url(${spotify})`}} className="shadow-ls shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
                     <div className="opacity-0 group-hover:opacity-100">
                         <span className="text-2xl font-bold text-white tracking-widee">
                             Spotify Clone Application
                         </span>
                         <div className="pt-8 text-center">
-                            <a href="https://github.com/l30-il-l30/spotify-desktop-app" className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg" rel="noopener noreferrer" target="_blank">
+                            <a href="https://github.com/l30-il-l30/spotify-desktop-app"
+                               className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+                               rel="noopener noreferrer" target="_blank">
                                 <button>Demo</button>
                             </a>
-                            <a href="https://github.com/l30-il-l30/spotify-desktop-app" className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg" rel="noopener noreferrer" target="_blank">
+                            <a href="https://github.com/l30-il-l30/spotify-desktop-app"
+                               className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+                               rel="noopener noreferrer" target="_blank">
+                                <button>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div style={{backgroundImage: `url(${booklet})`}} className="shadow-ls shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                    <div className="opacity-0 group-hover:opacity-100">
+                        <span className="text-2xl font-bold text-white tracking-widee">
+                            Booklet Online Book Store
+                        </span>
+                        <div className="pt-8 text-center">
+                            <a href="https://github.com/l30-il-l30/booklet-online-bookstor"
+                               className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+                               rel="noopener noreferrer" target="_blank">
+                                <button>Demo</button>
+                            </a>
+                            <a href="https://github.com/l30-il-l30/booklet-online-bookstor"
+                               className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+                               rel="noopener noreferrer" target="_blank">
                                 <button>Code</button>
                             </a>
                         </div>
